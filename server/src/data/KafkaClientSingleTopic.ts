a8531d714023a11eb851b0b390eabddef7aa96de
gitCraze2020 <jurgenstoop@yahoo.com>
2020-10-13 21:26:14 -0400

    First kafka to graphql subscription version - cleaned up a bit

--- server/src/data/KafkaClientSingleTopic.ts
+++ server/src/data/KafkaClientSingleTopic.ts
@@ -1,29 +1,20 @@
 import { Consumer } from "kafka-node";
 import {Job} from "../entities/Job";
 import {Publisher} from "type-graphql";
+
 interface KafkaClientSingleTopicOptions {
     topicName: string;
-    // onMessage: string;
-    // onMessage: (job: Job) => void;
     onMessage: (job: Job, pubsub_from_ctx: Publisher<Job>) => Promise<void>;
     pubsub: Publisher<Job>;
-    //onMessage: (...args: any[]) => void, options?: Object
-    // onMessage(): { (myArgument: string): string };
 }
 export class KafkaClientSingleTopic {
-    onMessage: (job: Job, pubsub_from_ctx: Publisher<Job>) => Promise<void>;
-    pubsub: Publisher<Job>;
-    topicName: string;
+    apiOptions: KafkaClientSingleTopicOptions;
     kafka = require('kafka-node');
-    Consumer = this.kafka.Consumer;
     client = new this.kafka.KafkaClient();
     consumerTopicJobs: Consumer;
 
-
     constructor(apiOptions: KafkaClientSingleTopicOptions) {
-        // var kafka = require('kafka-node'),
-        //     Consumer = kafka.Consumer,
-        //     client = new kafka.KafkaClient(),
+        this.apiOptions = apiOptions;
         this.consumerTopicJobs = new this.kafka.Consumer(
                 this.client,
                 [
@@ -34,58 +25,36 @@ export class KafkaClientSingleTopic {
                     autoCommit: false
                 }
             );
-        this.onMessage = apiOptions.onMessage;
-        this.consumerTopicJobs.on('error', function (err: any) { // for now, message is type any
+        this.consumerTopicJobs.on('error', function (err: any) { // for now, err is type any
             console.log(err);
         });
 
-        // this.consumerTopicJobs.on('message',  (message: any) => this.processInbound(message)); // for now, message is type any
-        this.consumerTopicJobs.on('message',  (message: any) => this.processInbound(message, apiOptions.pubsub)); // for now, message is type any
+        this.consumerTopicJobs.on('message',  (message: any) => this.processInbound(message, apiOptions.pubsub));
     }
 
     public toString() {
-        const info = "KafkaClientSingleTopic: " + typeof this.consumerTopicJobs;
+        const info = "KafkaClientSingleTopic: " + this.consumerTopicJobs
+            + "\n"
+            + "apiOptions: " + this.apiOptions;
         return info;
     }
 
     private async processInbound (message: any, pubsub: Publisher<Job>) { // for now, message is type any
-        console.log(message);
-        console.log(message.value);
         const sMsg: string = `${message.value}`
         const jsonFormatMessage = JSON.parse( sMsg );
-        console.log("jsonFormatMessage: ", jsonFormatMessage.id);
-        console.log("jsonFormatMessage id: ", jsonFormatMessage.id);
+
         var jobId: number = jsonFormatMessage.id;
         var jobName: string = jsonFormatMessage.name;
         const newJob: Job = { id: jobId, name: jobName };
-        console.log("kafkaClient running pubsub on: ", newJob);
+        console.log("KafkaClientSingleTopic running pubsub on: ", newJob);
+
         try {
-            this.onMessage(newJob, pubsub);
+            this.apiOptions.onMessage(newJob, pubsub);
         }
         catch (e) {
-            console.log("processInbound while sending to resolver caught error: ", e);
+            console.log("KafkaClientSingleTopic while sending to resolver caught error: ", e);
         }
     };
-
-    // consumerTopicJobs.on('message', function (message: any) { // for now, message is type any
-    //     console.log(message);
-    //     console.log(message.value);
-    //     const sMsg: string = `${message.value}`
-    //     const jsonFormatMessage = JSON.parse( sMsg );
-    //     console.log("jsonFormatMessage: ", jsonFormatMessage.id);
-    //     console.log("jsonFormatMessage id: ", jsonFormatMessage.id);
-    //     var jobId: number = jsonFormatMessage.id;
-    //     var jobName: string = jsonFormatMessage.name;
-    //     const newJob: Job = { id: jobId, name: jobName };
-    //     console.log("kafkaClient running pubsub on: ", newJob);
-    //
-    //     console.log("gqlServer: ", gqlServer);
-    //     if (!gqlServer) {
-    //         throw "kafka client needs graphql server property set";
-    //     }
-    //     gqlServer.executeOperation({ query: "query { hello }"});
-    //
-    // });
 
 }
 
--- server/src/resolvers/job.ts
+++ server/src/resolvers/job.ts
@@ -12,8 +12,6 @@ import {
     Publisher, PubSub
 } from "type-graphql";
 import { Job } from "../entities/Job";
-import { sampleJobs } from "..";
-import { Context } from "graphql-composer";
 import {KafkaClientSingleTopic} from "../data/KafkaClientSingleTopic";
 
 // import { sleep } from "../utils/sleep";
@@ -50,181 +48,61 @@ class JobResponse {
 @Resolver()
 export class JobResolver {
     kafkaClient: KafkaClientSingleTopic;
-    // psctx: any;
 
     initKafkaClient(publish: Publisher<Job>) {
-        // console.log("constructor, this.kafkaClient:", this.kafkaClient);
-        // console.log("initKafkaClient received ctx: ", ctx);
-        // this.psctx = ctx.pubsub_in_ctx;
-        // console.log("initKafkaClient set this.psctx: ", this.psctx);
         this.kafkaClient = new KafkaClientSingleTopic({topicName: "jobs", pubsub: publish, onMessage: this.onMessage});
     }
 
     async onMessage (job: Job, pubsub_from_ctx: Publisher<Job>) : Promise<void> {
-        console.log("onMessage received from kafka: " , job);
         try {
             console.log("onMessage pubsub_from_ctx: " , pubsub_from_ctx);
             await pubsub_from_ctx(job);
             console.log("onMessage published: " , job);
-            // await publish(job);
-            // await ctx.req.pubsub.publish('jobChangeSubscription', job);
         }
         catch (e) {
             console.log("onMessage caught error: " , e);
         }
-        // await Ctx.req.pubsub.publish('jobChangeSubscription', job);
     }
 
 
     @Query(() => [Job])
-    async jobs() : Promise<Job[]> {
-        // async jobs() {
-    //     await sleep(8000);
-        return sampleJobs;
+    async jobs(
+        @PubSub("jobChangeSubscription") publish: Publisher<Job>,
+    ) : Promise<Job[]> {
+        if (!this.kafkaClient) {this.initKafkaClient(publish);}
+        // rather than returning data here,
+        // reset the offset on kafka, which will trigger the subscription
+        return [];
     }
 
 
     @Query(() => String)
     async hello(
-        // @Ctx() ctx: any,
         @PubSub("jobChangeSubscription") publish: Publisher<Job>,
     ) {
-        //await ctx.req.pubsub.publish('jobChangeSubscription');
-        this.initKafkaClient(publish);
+        if (!this.kafkaClient) {this.initKafkaClient(publish);}
         return "Initialized kafka client!";
     }
-
-    @Mutation(() => JobResponse)
-    async updateJobName (
-        @Arg("id", () => Int) jobId: number,
-        @Arg("name") jobName: string
-    ) : Promise<JobResponse | null> {
-        // return sampleJobs[0]
-        let idx : number = -1;
-        var BreakException = {};
-        try {
-            sampleJobs.forEach(job => {
-                idx += 1;
-                if (job.id === jobId) {
-                    throw BreakException;
-                };
-            });
-        } catch (e) {
-            if (e !== BreakException) {
-                return {
-                    errors: [{
-                        field: "id",
-                        message: e.message
-                    }]
-                }
-            };
-        }
-        if (idx === -1) {
-            return {
-                errors: [{
-                    field: "id",
-                    message: "No jobs found"
-                }]
-            }
-        }
-        if (idx === sampleJobs.length) {
-            return {
-                errors: [{
-                    field: "id",
-                    message: `No job found with id ${jobId}`
-                }]
-            }
-        }
-        sampleJobs[idx].name = jobName;
-
-        return {
-            job: sampleJobs[idx]
-        };
-        // .filter((job: Job) => job.id === jobId)
-    }
-
-
-// console.log("updateJobName running pubsub on: ", sampleJobs[idx]);
-//         // Publish the job so that the subscription can fire off
-//         pubsub.publish('jobChangeSubscription', {
-//             jobChangeSubscription: { ...sampleJobs[idx], jobName },
-//         });
-
 
     @Mutation(() => JobResponse)
     async updateJob (
         @Arg("jobToUpdate") jobUpdate: JobInput,
         @PubSub("jobChangeSubscription") publish: Publisher<Job>,
     ) : Promise<JobResponse | null> {
-        // return sampleJobs[0]
-        let idx : number = -1;
-        var BreakException = {};
-        try {
-            sampleJobs.forEach(job => {
-                idx += 1;
-                if (job.id === jobUpdate.id) {
-                    throw BreakException;
-                };
-            });
-        } catch (e) {
-            if (e !== BreakException) {
-                return {
-                    errors: [{
-                        field: "id",
-                        message: e.message
-                    }]
-                }
-            };
-        }
-        if (idx === -1) {
-            return {
-                errors: [{
-                    field: "id",
-                    message: "No jobs found"
-                }]
-            }
-        }
-        if (sampleJobs[idx].id != jobUpdate.id) {
-            return {
-                errors: [{
-                    field: "id",
-                    message: `No job found with id ${jobUpdate.id}`
-                }]
-            }
-        }
-        sampleJobs[idx].name = jobUpdate.name;
+        if (!this.kafkaClient) {this.initKafkaClient(publish);}
 
-        console.log("updateJob running pubsub on: ", sampleJobs[idx]);
+        console.log("updateJob running pubsub on: ", jobUpdate);
         // Publish the job so that the subscription can fire off
-        const payload: Job = sampleJobs[idx];
+        const payload: Job = jobUpdate;
         await publish(payload);
         // await pubsub.publish("jobChangeSubscription", payload);
         // pubsub.publish('jobChangeSubscription', { jobChangeSubscription: { ...sampleJobs[idx] },});
         console.log("updateJob ran pubsub.publish of: ", payload);
 
-        return {
-            job: sampleJobs[idx]
-        };
-        // .filter((job: Job) => job.id === jobId)
+        // rather than returning data here,
+        // reset the offset on kafka, which will trigger the subscription
+        return { job: jobUpdate };
     }
-
-    @Query(() => Job)
-    ctxBody(context: Context) {
-        console.log("context.body before:", context.body);
-
-        context.body = new Job();
-
-        console.log("context.body after:", context.body);
-
-    }
-
-    // @PubSub("jobChangeSubscription") pubsub: Publisher<Job>,
-    // GetJobChangesSubscription: {
-    //     jobChangeSubscription: {
-    //         subscribe: () => pubsub.asyncIterator("jobChangeSubscription"),
-    //     },
-    // }
-    //
 
     @Subscription({
         topics: "jobChangeSubscription",
@@ -246,16 +124,7 @@ export class JobResolver {
         }
         return jobPayload;
     }
-
-    // @Query()
-    // trigger(): JobResponse {
-    //     pubsub.publish('jobChange', { job: sampleJobs[idx] });
-    //     // ...
-    // }
-    //
-    //     jobChange: {
-    //         subscribe: () => (return sampleJobs[idx])
-    //         // subscribe: () => pubsub.asyncIterator(['jobChange']),
-    //     },
-    // }
 }
+
+
+
